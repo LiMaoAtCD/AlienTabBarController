7247cba701b511c6ead3a9c46672bbe4cbf4bc70
AlienLi <liamscofield@icloud.com>
2014-08-27 11:56:01 +0800

    Initial Commit

---
+++ tabBarController/ALIENAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ALIENAppDelegate.h
+//  tabBarController
+//
+//  Created by AlienLi on 14-8-27.
+//  Copyright (c) 2014年 AlienLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALIENAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
